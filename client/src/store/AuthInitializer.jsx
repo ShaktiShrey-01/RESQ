0cc93481fa9ed3d5938810be1478721cad557251
Shakti <shrey@Shaktis-MacBook-Air.local>
2026-08-24 16:08:52 +0530

    chnaged

--- client/src/store/AuthInitializer.jsx
+++ client/src/store/AuthInitializer.jsx
@@ -12,7 +12,7 @@ function AuthInitializer({ children }) {
         const checkLogin = async () => {
             try {
                 const res = await axios.post(
-                    "http://localhost:8000/api/users/refreshtoken", 
+                    `${import.meta.env.VITE_BACKEND_URL}/api/users/refreshtoken`, 
                     {}, 
                     { withCredentials: true }
                 );
